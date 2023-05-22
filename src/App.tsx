b64af10adb504c8d81bfeee32611482122b05937
Quentin Michalet <qmichalet@mini-de-quentin.home>
2023-05-22 15:52:07 +0200

    new navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,26 @@ import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar";
 
 function App() {
-  const theme = useMemo(() => createTheme(themeSettings), []);
-  return (
-    <div className="app">
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
-              <Route path="/predictions" element={<div>predictions page</div>} />
-            </Routes>
-           </Box>
-        </ThemeProvider>
-      </BrowserRouter>
-    </div>
-  );
+	const theme = useMemo(() => createTheme(themeSettings), []);
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+						<Navbar />
+						<Routes>
+							<Route path="/" element={<div>dashboard page</div>} />
+							<Route
+								path="/predictions"
+								element={<div>predictions page</div>}
+							/>
+						</Routes>
+					</Box>
+				</ThemeProvider>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;

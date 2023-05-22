e2f52ecab0493b6ab08b7c087f72736d703ed602
Quentin <33957011+qmichalet@users.noreply.github.com>
2023-05-22 16:52:21 +0200

    Feature : dashboard layout (#2)
    Co-authored-by: Quentin Michalet <qmichalet@mini-de-quentin.home>

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useMemo } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
 	const theme = useMemo(() => createTheme(themeSettings), []);
@@ -15,7 +16,7 @@ function App() {
 					<Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
 						<Navbar />
 						<Routes>
-							<Route path="/" element={<div>dashboard page</div>} />
+							<Route path="/" element={<Dashboard />} />
 							<Route
 								path="/predictions"
 								element={<div>predictions page</div>}

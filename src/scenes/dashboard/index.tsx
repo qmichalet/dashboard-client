603818e523d0908532aacdf5acf312455eafd65f
Quentin <33957011+qmichalet@users.noreply.github.com>
2023-05-22 18:52:37 +0200

    Config : redux toolkit setup (#3)
    Co-authored-by: Quentin Michalet <qmichalet@mini-de-quentin.home>

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -3,52 +3,49 @@ import Row1 from "./Row1";
 import Row2 from "./Row2";
 import Row3 from "./Row3";
 
-type Props = {};
-
 const gridTemplateLargeScreens = `
-    "a b c"
-    "a b c"
-    "a b c"
-    "a b f"
-    "d e f"
-    "d e f"
-    "d h i"
-    "g h i"
-    "g h j"
-    "g h j"
+  "a b c"
+  "a b c"
+  "a b c"
+  "a b f"
+  "d e f"
+  "d e f"
+  "d h i"
+  "g h i"
+  "g h j"
+  "g h j"
 `;
-
 const gridTemplateSmallScreens = `
-    "a"
-    "a"
-    "a"
-    "a"
-    "b"
-    "b"
-    "b"
-    "b"
-    "c"
-    "c"
-    "c"
-    "d"
-    "d"
-    "d"
-    "e"
-    "e"
-    "f"
-    "f"
-    "f"
-    "g"
-    "g"
-    "g"
-    "h"
-    "h"
-    "h"
-    "h"
-    "i"
-    "i"
-    "j"
-    "j"
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
 `;
 
 const Dashboard = () => {
@@ -62,14 +59,14 @@ const Dashboard = () => {
 			sx={
 				isAboveMediumScreens
 					? {
-							gridTemplateAreas: gridTemplateLargeScreens,
 							gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-							gridTemplateRows: "repeat(10, minmax(60px, 1fr))"
+							gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+							gridTemplateAreas: gridTemplateLargeScreens
 					  }
 					: {
-							gridTemplateAreas: gridTemplateSmallScreens,
-							gridTemplateColumns: "1fr",
-							gridTemplateRows: "80px"
+							gridAutoColumns: "1fr",
+							gridAutoRows: "80px",
+							gridTemplateAreas: gridTemplateSmallScreens
 					  }
 			}
 		>

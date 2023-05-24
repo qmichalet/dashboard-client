77cc1b26317711101a0addf165c0658b5ee107fc
Quentin <33957011+qmichalet@users.noreply.github.com>
2023-05-24 17:00:57 +0200

    Feature : profit and revenue chart (#5)
    Co-authored-by: Quentin Michalet <qmichalet@mini-de-quentin.home>

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -6,11 +6,14 @@ import { useMemo } from "react";
 import {
 	ResponsiveContainer,
 	AreaChart,
-	CartesianGrid,
 	XAxis,
 	YAxis,
 	Tooltip,
-	Area
+	Area,
+	Line,
+	CartesianGrid,
+	Legend,
+	LineChart
 } from "recharts";
 
 type Props = {};
@@ -27,6 +30,19 @@ const Row1 = (props: Props) => {
 					name: month.substring(0, 3),
 					revenue,
 					expenses
+				};
+			})
+		);
+	}, [data]);
+
+	const revenueProfit = useMemo(() => {
+		return (
+			data &&
+			data[0].monthlyData.map(({ month, revenue, expenses }) => {
+				return {
+					name: month.substring(0, 3),
+					revenue,
+					profit: (revenue - expenses).toFixed(2)
 				};
 			})
 		);
@@ -109,7 +125,58 @@ const Row1 = (props: Props) => {
 					</AreaChart>
 				</ResponsiveContainer>
 			</DashboardBox>
-			<DashboardBox gridArea="b"></DashboardBox>
+			<DashboardBox gridArea="b">
+				<BoxHeader
+					title="Profit and Revenue"
+					subtitle="top line represents revenue, bottom line represents expenses"
+					sideText="+4%"
+				/>
+				<ResponsiveContainer width="100%" height="100%">
+					<LineChart
+						data={revenueProfit}
+						margin={{
+							top: 20,
+							right: 0,
+							left: -10,
+							bottom: 55
+						}}
+					>
+						<CartesianGrid vertical={false} stroke={palette.grey[800]} />
+						<XAxis
+							dataKey="name"
+							tickLine={false}
+							style={{ fontSize: "10px" }}
+						/>
+						<YAxis
+							yAxisId="left"
+							tickLine={false}
+							axisLine={false}
+							style={{ fontSize: "10px" }}
+						/>
+						<YAxis
+							yAxisId="right"
+							orientation="right"
+							tickLine={false}
+							axisLine={false}
+							style={{ fontSize: "10px" }}
+						/>
+						<Tooltip />
+						<Legend height={20} wrapperStyle={{ margin: "0 0 10px 0" }} />
+						<Line
+							yAxisId="left"
+							type="monotone"
+							dataKey="profit"
+							stroke={palette.tertiary[500]}
+						/>
+						<Line
+							yAxisId="right"
+							type="monotone"
+							dataKey="revenue"
+							stroke={palette.primary.main}
+						/>
+					</LineChart>
+				</ResponsiveContainer>
+			</DashboardBox>
 			<DashboardBox gridArea="c"></DashboardBox>
 		</>
 	);

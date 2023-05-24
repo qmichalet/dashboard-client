521ffc771e131ad997c1e2d7ab8a9adbadc9c638
Quentin Michalet <qmichalet@mini-de-quentin.home>
2023-05-24 15:59:26 +0200

    box header component

---
+++ src/components/BoxHeader.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Typography, useTheme, Box } from "@mui/material";
+import FlexBetween from "./FlexBetween";
+
+type Props = {
+	title: string;
+	subtitle?: string;
+	icon?: React.ReactNode;
+	sideText: string;
+};
+
+const BoxHeader = ({ icon, title, subtitle, sideText }: Props) => {
+	const { palette } = useTheme();
+	return (
+		<FlexBetween margin="1.5rem 1rem 0rem" color={palette.grey[400]}>
+			<FlexBetween>
+				{icon}
+				<Box width="100%">
+					<Typography variant="h4" mb="-0.1rem">
+						{title}
+					</Typography>
+					<Typography variant="h6">{subtitle}</Typography>
+				</Box>
+			</FlexBetween>
+			<Typography variant="h4" fontWeight="700" color={palette.secondary[500]}>
+				{sideText}
+			</Typography>
+		</FlexBetween>
+	);
+};
+
+export default BoxHeader;

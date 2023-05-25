66ae2dd32732d6905e238ce93ca0ca4d37d55f99
Quentin <33957011+qmichalet@users.noreply.github.com>
2023-05-25 14:09:17 +0200

    Feature : handle new products api route and fixed indent (#8)
    Co-authored-by: Quentin Michalet <qmichalet@mini-de-quentin.home>

--- src/state/types.ts
+++ src/state/types.ts
@@ -1,33 +1,44 @@
 export interface ExpensesByCategory {
-	salaries: number;
-	supplies: number;
-	services: number;
+  salaries: number;
+  supplies: number;
+  services: number;
 }
 
 export interface Month {
-	id: string;
-	month: string;
-	revenue: string;
-	expenses: string;
-	nonOperationalExpenses: string;
-	operationalExpenses: string;
+  id: string;
+  month: string;
+  revenue: string;
+  expenses: string;
+  nonOperationalExpenses: string;
+  operationalExpenses: string;
 }
 
 export interface Day {
-	id: string;
-	date: string;
-	revenue: string;
-	expenses: string;
+  id: string;
+  date: string;
+  revenue: string;
+  expenses: string;
 }
 
 export interface GetKpisReponse {
-	id: string;
-	_id: string;
-	__v: number;
-	totalProfit: number;
-	totalRevenue: number;
-	totalExpenses: number;
-	expensesByCategory: ExpensesByCategory;
-	monthlyData: Array<Month>;
-	dailyData: Array<Day>;
+  id: string;
+  _id: string;
+  __v: number;
+  totalProfit: number;
+  totalRevenue: number;
+  totalExpenses: number;
+  expensesByCategory: ExpensesByCategory;
+  monthlyData: Array<Month>;
+  dailyData: Array<Day>;
+}
+
+export interface GetProductsReponse {
+  id: string;
+  _id: string;
+  __v: number;
+  price: number;
+  expense: number;
+  transactions: Array<String>;
+  createdAt: string;
+  updatedAt: string;
 }

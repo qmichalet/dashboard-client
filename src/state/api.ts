801cacf9c52c0c8f7469d4c9fee465c12c22f846
Quentin Michalet <qmichalet@mini-de-quentin.home>
2023-05-25 14:07:37 +0200

    handle new products api route and fixed indent

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,16 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpisReponse } from "./types";
+import { GetKpisReponse, GetProductsReponse } from "./types";
 
 export const api = createApi({
-	baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
-	reducerPath: "main",
-	tagTypes: ["Kpis"],
-	endpoints: (build) => ({
-		getKpis: build.query<Array<GetKpisReponse>, void>({
-			query: () => "kpi/kpis/",
-			providesTags: ["Kpis"]
-		})
-	})
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: ["Kpis", "Products"],
+  endpoints: (build) => ({
+    getKpis: build.query<Array<GetKpisReponse>, void>({
+      query: () => "kpi/kpis/",
+      providesTags: ["Kpis"]
+    }),
+    getProducts: build.query<Array<GetProductsReponse>, void>({
+      query: () => "product/products",
+      providesTags: ["Products"]
+    })
+  })
 });
 
-export const { useGetKpisQuery } = api;
+export const { useGetKpisQuery, useGetProductsQuery } = api;
